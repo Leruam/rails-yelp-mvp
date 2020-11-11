fcc5804b2a0e0f5733aefa5608ad59b9ba3f5cb7
antoine maurel <antoine.maurel@gmail.com>
2020-11-11 15:32:33 +0100

    done all the methods and routes but not the links and the formating

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,17 +9,28 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    @restaurant = Restaurant.new(params_restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+      # permet de reafficher le form avec les possibilites de simple_form
+      # comme le fait d'avoir des averstissements sur les validates
+    end
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def params_restaurant
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,33 @@
 class ReviewsController < ApplicationController
 
   def new
-
+    set_restaurant
+    @review = Review.new
   end
 
   def create
+    set_restaurant
+    @review = Review.new(params_review)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def set_review
+    @restaurant = Review.find(params[:id])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def params_review
+    params.require(:review).permit(:rating, :content)
   end
 
 end
